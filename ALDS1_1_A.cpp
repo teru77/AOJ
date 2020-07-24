c5862bc9695644d40f3c6355af2a5bf59e9db434
uret <teruyongsen258@gmail.com>
2020-07-24 15:11:09 +0900

    first commit

---
+++ ALDS1_1_A.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n,v,j;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)cin>>a[i];
+    for(int i=0;i<n;i++){
+        v = a[i];
+        j=i-1;
+        while(j>=0 && a[j]>v){
+            a[j+1] = a[j];
+                j--;
+        }
+        a[j+1]=v;
+    for(int i=0;i<n;i++){
+    if(i!=n-1)cout<<a[i]<<" ";
+    else cout<<a[i];
+    }
+    cout<<endl;
+    }
+}
